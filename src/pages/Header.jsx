7ee78e19a805ec0e14fd5450737593bdd0adadb9
Naeem Ashraf <123843315+NP5555@users.noreply.github.com>
2024-09-20 21:18:42 +0500

    Update Header.jsx

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="items-center ">
+    <div className="items-center p-4">
       <h1 className="text-2xl sm:text-3xl font-bold mt-3 rounded-xl  text-purple-600 bg-white text-center">
         AGE CALCULATOR
       </h1>
