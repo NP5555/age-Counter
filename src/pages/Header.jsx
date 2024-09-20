d60cf1475f684ef921b0e597776cf682d19dd4de
Naeem Ashraf <123843315+NP5555@users.noreply.github.com>
2024-09-20 12:37:50 -0700

    Finalize stage Commit"

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="items-center ">
-      <h1 className="text-2xl sm:text-3xl font-bold mt-3 rounded-xl  text-purple-600 bg-white text-center">
+    <div className="items-center">
+      <h1 className="text-xl sm:text-2xl font-bold rounded-xl  text-purple-600 bg-white text-center">
         AGE CALCULATOR
       </h1>
     </div>
